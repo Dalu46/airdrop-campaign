4443cf03a963b7de57a3cc4722f42a72696f6665
dalu46 <lawrencefranklin100@gmail.com>
2023-01-26 02:02:12 +0100

    {

--- src/views/dashboard/social media/SocialMedia.js
+++ src/views/dashboard/social media/SocialMedia.js
@@ -1,11 +1,36 @@
 import React from "react";
+import shape from "../../../assets/images/Shape.svg";
 
-const SocialMedia = () => {
-    return (
-        <div className="social-media">
-            SocialMedia
-        </div>
-    )
-}
+const SocialMedia = ({ socialMediaIcon, socialMediaName }) => {
+  const verfied = "Not verrified";
+  return (
+    <div className="social-media">
+      <div className="verrification-div">
+        <img
+          className="social-media-image"
+          src={socialMediaIcon}
+          alt="verify with twitter"
+        />
+        <span>
+          <p>Status: {verfied}</p>
+          <img src={shape} alt="your account has been verifed" />
+        </span>
+      </div>
+
+      <div className="social-media-name-div">
+        <p className="social-media-name">{socialMediaName}</p>
+        <p className="follow-social-media">
+          Make sure you follow @Manillafinance on {socialMediaName} before you input your
+          username
+        </p>
+      </div>
+
+      <div className="connect-buttons-div">
+        <input type="text" className="user-name-input" placeholder={`input ${socialMediaName} username`}/>
+        <button>Verify</button>
+      </div>
+    </div>
+  );
+};
 
 export default SocialMedia;
