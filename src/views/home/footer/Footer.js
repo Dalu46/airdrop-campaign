7b8766f078b9eb3114e7433174bff0114b7d4280
dalu46 <lawrencefranklin100@gmail.com>
2023-02-05 00:17:42 +0100

    Added social media links

--- src/views/home/footer/Footer.js
+++ src/views/home/footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = ({ darkText, darkLogo, isMobile }) => {
   /* <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> */
 
   return (
-    <div className={`footer ${isMobile && 'mobile-footer'} `}>
+    <div className={`footer ${isMobile && "mobile-footer"} `}>
       <div className="footer-wrapper">
         <div className="mnla-logo-div">
           {darkLogo ? (
@@ -25,35 +25,55 @@ const Footer = ({ darkText, darkLogo, isMobile }) => {
           ) : (
             <img src={Logo} alt="MNLA Logo" />
           )}
-          <p className={`intuivite-paragraph ${darkText && 'dark-text'}`}>
+          <p className={`intuivite-paragraph ${darkText && "dark-text"}`}>
             Intuitive Interface, Lightning speed transactions, Advanced Security
             and 24H support.
           </p>
-          <p className={`mnla-technologies ${darkText && 'dark-text'}`}>© Manilla Tecgnologies 2023</p>
+          <p className={`mnla-technologies ${darkText && "dark-text"}`}>
+            © Manilla Finance 2023
+          </p>
         </div>
 
         <div className="social-media-div">
           <div className="icons-div">
-            <span className="imgs">
-            <img className="icons-img" src={facebook} alt="Facebook's logo" />
-            </span>
 
-            <span className="imgs">
-            <img className="icons-img" src={linkedin} alt="linkedin's logo" />
-            </span>
+            <a href="https://www.linkedin.com/company/manilla-technologies/">
+              {" "}
+              <span className="imgs">
+                <img
+                  className="icons-img"
+                  src={linkedin}
+                  alt="linkedin's logo"
+                />
+              </span>
+            </a>
 
-            <span className="imgs">
-            <img className="icons-img" src={twitter} alt="Twitter's logo" />
-            </span>
+            <a href="http://twitter.com/manilla_finance">
+              <span className="imgs">
+                <img className="icons-img" src={twitter} alt="Twitter's logo" />
+              </span>
+            </a>
 
-            <span className="imgs">
-            <img className="icons-img" src={instagram} alt="Instagram's logo" />
-            </span>
+            <a href="http://instagram.com/manillafinance_">
+              <span className="imgs">
+                <img
+                  className="icons-img"
+                  src={instagram}
+                  alt="Instagram's logo"
+                />
+              </span>{" "}
+            </a>
 
-            <span className="imgs">
-            <img className="icons-img" src={telegram} alt="Telegram's logo" />
-            </span>
-            
+            <a href="http://t.me/manilla_finance">
+              {" "}
+              <span className="imgs">
+                <img
+                  className="icons-img"
+                  src={telegram}
+                  alt="Telegram's logo"
+                />
+              </span>
+            </a>
           </div>
           <div className="translation">
             <div className="tranlation-text">English</div>
--- src/views/home/home video/HomeVideo.js
+++ src/views/home/home video/HomeVideo.js
@@ -5,15 +5,17 @@ const HomeVideo = () => {
   return (
     <div className="video-box">
       <div className="video-container">
-          <iframe
-            class="iframe-video"
-            src="https://www.youtube.com/embed/kD-IIeri2Uo"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        <button>Learn more about this project</button>
+        <iframe
+          class="iframe-video"
+          src="https://www.youtube.com/embed/kD-IIeri2Uo"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+        <a href="https://manilla.finance/">
+          <button className="learn-btn">Learn more about this project</button>
+        </a>
       </div>
     </div>
   );
