8657611461873c303bf528a17ddd1ab8ac966383
dalu46 <lawrencefranklin100@gmail.com>
2023-02-21 18:38:11 +0100

    Added airdro countdwon function

--- src/components/airdrop/AirDrop.js
+++ src/components/airdrop/AirDrop.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import "./airdrop.css";
 
 const AirDrop = () => {
-  const days = "07";
-  const hours = 20;
-  const minutes = 30;
-  const seconds = 45;
+
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countDownDate = new Date('May 24, 2023 00:00:00').getTime();
+    console.log()
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)))
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        // stop timer
+        clearInterval(interval.current)
+      } else {
+        // update timer 
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds)
+      }
+    })
+    
+  }
+
+  // once the component mounts
+  useEffect(() => {
+    startTimer();
+  }, [])
+
+  // const days = "07";
+  // const hours = 20;
+  // const minutes = 30;
+  // const seconds = 45;
 
   return (
     <div className="main-air-drop-container">
@@ -15,7 +55,7 @@ const AirDrop = () => {
           <p className="air-text">Airdrop Claim Portal Opens In</p>
           <div className="count-down">
             <div className="date-box">
-              <p className="date-box-num">{days}</p>
+              <p className="date-box-num">{timerDays}</p>
               <p className="day">days</p>
             </div>
 
@@ -25,7 +65,7 @@ const AirDrop = () => {
             </span>
 
             <div className="date-box">
-              <p className="date-box-num">{hours}</p>
+              <p className="date-box-num">{timerHours}</p>
               <p className="day">hours</p>
             </div>
 
@@ -35,7 +75,7 @@ const AirDrop = () => {
             </span>
 
             <div className="date-box">
-              <p className="date-box-num">{minutes}</p>
+              <p className="date-box-num">{timerMinutes}</p>
               <p className="day">minutes</p>
             </div>
 
@@ -45,7 +85,7 @@ const AirDrop = () => {
             </span>
 
             <div className="date-box">
-              <p className="date-box-num">{seconds}</p>
+              <p className="date-box-num">{timerSeconds}</p>
               <p className="day">seconds</p>
             </div>
           </div>
